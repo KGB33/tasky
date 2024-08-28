09d87aa73628370c95547d73ce2b453b1be831ec
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2024-08-28 15:09:16 -0700

    inital commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    crane,
+    flake-utils,
+    nixpkgs,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
+        };
+        craneLib =
+          (crane.mkLib pkgs).overrideToolchain (p:
+            pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
+      in rec
+      {
+        packages = {
+          tasky = craneLib.buildPackage {
+            src = craneLib.cleanCargoSource ./.;
+            doCheck = false; # TODO: Add example files to check environment so the tests pass.
+          };
+          default = packages.tasky;
+        };
+        devShells = {
+          default = craneLib.devShell {
+            inputsFrom = [packages.default];
+            packages = with pkgs; [rust-analyzer];
+          };
+        };
+      }
+    );
+}
